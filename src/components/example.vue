4711c4e7e201e7a43b02e36cbc4d5d6c93ffab3d
André Gonçalves <Startrule@gmail.com>
2021-09-05 15:51:29 +0100

    added some structure to home

---
+++ src/components/example.vue
@@ -0,0 +1,20 @@
+<template>
+    <button @click="handler">click me to test fetch</button>
+    <div v-if="loadComplete"> {{t('message')}} </div>
+</template>
+
+<script setup>
+import { useI18n } from 'vue-i18n'
+import { ref, inject } from 'vue'
+const { t } = useI18n()
+const axios = inject('axios')
+
+const loadComplete = ref(false)
+
+const handler = async () => {
+    try {
+        await axios.get('https://my-hub-backend.herokuapp.com/users')
+        loadComplete.value = true
+    } catch (err) {}
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,15 @@
 <template>
     <div class="home">
-        <button @click="handler">click me to test fetch</button>
-        <div v-if="loadComplete"> {{t('message')}} </div>
+        <app-header/>
+        <app-content/>
+        <app-footer/>
     </div>
 </template>
 
 <script setup>
-import { useI18n } from 'vue-i18n'
-import { reactive, ref, inject } from 'vue'
-const { t } = useI18n()
-const axios = inject('axios')
-
-const loadComplete = ref(false)
-
-const handler = async () => {
-    try {
-        await axios.get('https://my-hub-backend.herokuapp.com/users')
-        loadComplete.value = true
-    } catch (err) {}
-}
+import AppHeader from '@/components/home/Header'
+import AppContent from '@/components/home/Content'
+import AppFooter from '@/components/home/Footer'
 </script>
 
 <style></style>
